699b584fa536c539cfff7b2d2780b07c62ddc8fa
P001493 <P001493@SPSTR-P001493.prodam.local>
2018-09-14 08:47:42 -0400

    Municipio

---
+++ src/main/java/br/gov/am/prodam/infracao/controller/MunicipioController.java
@@ -0,0 +1,63 @@
+package br.gov.am.prodam.infracao.controller;
+
+import java.util.List;
+
+import javax.validation.Valid;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import br.gov.am.prodam.infracao.domain.Municipio;
+import br.gov.am.prodam.infracao.dto.MunicipioDTO;
+import br.gov.am.prodam.infracao.service.MunicipioService;
+
+@Component
+@Path("/municipio")
+public class MunicipioController extends BasicController{
+	
+	@Autowired
+	private MunicipioService municipioService;
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<MunicipioDTO> findAll(){
+		List<Municipio> municipios = municipioService.findAll();
+		return mapList(municipios, MunicipioDTO.class);
+	}
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("{infracaoId}")
+	public MunicipioDTO findById(@PathParam("infracaoId") Long id) {
+		Municipio municipio = municipioService.findById(id);
+		return map(municipio, MunicipioDTO.class);
+	}
+	
+	@DELETE
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("{infracaoId}")
+	public Response delete(@PathParam("infracaoId") Long id) {
+		municipioService.delete(id);
+		return Response.ok().build();
+	}
+	
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response salvar(@Valid MunicipioDTO dto) {
+		Municipio municipio = map(dto, Municipio.class);
+		municipioService.salvar(municipio);
+		return Response.ok().build();
+	}
+	
+	
+}
---
+++ src/main/java/br/gov/am/prodam/infracao/domain/Municipio.java
@@ -0,0 +1,69 @@
+package br.gov.am.prodam.infracao.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name="MUNICIPIO")
+public class Municipio {
+	
+	@Id
+	private Long id;
+	
+	@Column(name="NOME", length=50, nullable=false)
+	@NotNull
+	private String nome;
+	
+	@Column(name="DESCRICAO", length=250, nullable=true)
+	private String descricao;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Municipio other = (Municipio) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}	
+
+}
