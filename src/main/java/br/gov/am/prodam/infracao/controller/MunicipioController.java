8e571a625b2d83c7b9dd239340b1829dc8a53e88
P001493 <P001493@SPSTR-P001493.prodam.local>
2018-09-27 13:39:27 -0400

    método atualizar em municipio

--- src/main/java/br/gov/am/prodam/infracao/controller/MunicipioController.java
+++ src/main/java/br/gov/am/prodam/infracao/controller/MunicipioController.java
@@ -9,6 +9,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -78,5 +79,16 @@ public class MunicipioController extends BasicController {
 				.build();
 		return created("Municipio salvo com sucesso", location);
 	}
+	
+	@PUT
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Path("{municipioID}")
+	public Response atualizar(@PathParam("municipioID") long id,  @Valid MunicipioDTO municipioDTO) {
+		Municipio municipio = map(municipioDTO, Municipio.class);
+		municipio.setId(id);
+		municipioService.save(municipio);
+		return ok("Municipio salvo com sucesso!");
+		}
 
 }
