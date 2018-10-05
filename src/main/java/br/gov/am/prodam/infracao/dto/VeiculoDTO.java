d69372b46381c18fd45837ca996ca7b76d7942b2
wrffilho <P001496@DIDES-P001458p.prodam.local>
2018-10-05 09:04:07 -0400

    alterações: inclusão dos atributos nome, marca, cor e foto

--- src/main/java/br/gov/am/prodam/infracao/dto/VeiculoDTO.java
+++ src/main/java/br/gov/am/prodam/infracao/dto/VeiculoDTO.java
@@ -11,6 +11,40 @@ public class VeiculoDTO {
 	private String renavam;
 	
 	private String foto;
+	
+	private int anoModelo;
+	
+	private String cor;
+	
+	private String marca;
+	
+	private String nome;
+	
+	
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public String getCor() {
+		return cor;
+	}
+
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
 
 	public Long getId() {
 		return id;
@@ -43,6 +77,80 @@ public class VeiculoDTO {
 	public void setFoto(String foto) {
 		this.foto = foto;
 	}
+
+	public int getAnoModelo() {
+		return anoModelo;
+	}
+
+	public void setAnoModelo(int anoModelo) {
+		this.anoModelo = anoModelo;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + anoModelo;
+		result = prime * result + ((cor == null) ? 0 : cor.hashCode());
+		result = prime * result + ((foto == null) ? 0 : foto.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((marca == null) ? 0 : marca.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result + ((placa == null) ? 0 : placa.hashCode());
+		result = prime * result + ((renavam == null) ? 0 : renavam.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		VeiculoDTO other = (VeiculoDTO) obj;
+		if (anoModelo != other.anoModelo)
+			return false;
+		if (cor == null) {
+			if (other.cor != null)
+				return false;
+		} else if (!cor.equals(other.cor))
+			return false;
+		if (foto == null) {
+			if (other.foto != null)
+				return false;
+		} else if (!foto.equals(other.foto))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (marca == null) {
+			if (other.marca != null)
+				return false;
+		} else if (!marca.equals(other.marca))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (placa == null) {
+			if (other.placa != null)
+				return false;
+		} else if (!placa.equals(other.placa))
+			return false;
+		if (renavam == null) {
+			if (other.renavam != null)
+				return false;
+		} else if (!renavam.equals(other.renavam))
+			return false;
+		return true;
+	}
+
+	
 	
 
 }
